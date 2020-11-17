473c326ecc5de8a393ed7add7fc9ada6f8ed78bf
Alex@WIFI <user@WIN10DEV>
2020-11-17 21:28:23 +0100

    Vererbungsbeispiel

---
+++ src/2020_11_17/EmployeeBeispiel/EmployeeBeispiel/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeBeispiel
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee employee = new Employee("Gustav", "Gans", new DateTime(2000, 11, 16), 50000);
+
+            Console.WriteLine(employee.GetInfoString());
+        }
+    }
+}
