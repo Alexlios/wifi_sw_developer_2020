154177792fbf5a41703a7e9f9ea9ad1dc5635443
Alex@WIFI <user@WIN10DEV>
2020-12-01 21:42:13 +0100

    moin

---
+++ src/2020_12_01/EventsGrundlagen/EventsGrundlagen/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventsGrundlagen
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var myCar = new Car("Super Car VW", 220);
+
+            myCar.CarExploded += MyCarExploded;
+
+            do
+            {
+                Console.WriteLine(myCar);
+                myCar.SpeedUp(20);
+                Console.ReadKey(true);
+            }
+            while (true);
+        }
+
+        private static void MyCarExploded(int currentSpeed, int maxSpeed)
+        {
+            Console.WriteLine($"\nBoom!!!!\n\n{currentSpeed}/{maxSpeed} km/h");
+            Environment.Exit(0);
+        }
+    }
+}
