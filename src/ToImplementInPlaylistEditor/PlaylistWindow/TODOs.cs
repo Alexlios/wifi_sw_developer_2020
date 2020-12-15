96bd8097f38122633bb6ac1b3f946e160808b500
Alex@WIFI <user@WIN10DEV>
2020-12-15 20:15:17 +0100

    hmmm...

--- src/ToImplementInPlaylistEditor/PlaylistWindow/TODOs.cs
+++ src/ToImplementInPlaylistEditor/PlaylistWindow/TODOs.cs
@@ -12,17 +12,4 @@
  * - [Class].operator [+/-/x//] ([toUse])
  * 
  * 
- * 
- * 
- * 
- * - aufbau abschauen für switchKonfigurator!!
- *   
- *   : Executer, der nur commands ausfuehrt
- *   : connection darf nicht switchspezifisch sein
- *     : in ecs2100-klasse die connection als output verwenden
- *     : in ecs2100-klasse bsp: executer("copy tftp file", connection);
- *   : in ecs2100-klasse die command-abschnitte in eigene methoden
- *   : MinimumSize für das Fenster
- *   : Files @"C:\Bla\Bla\Bla"
- * 
  */
